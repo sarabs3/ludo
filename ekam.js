91cfa3041f94dc82a921fed4a8f3183ef864b85b
sarabjeet singh <sarabjeetsingh@sarabjeets-MacBook-Air.local>
2025-12-17 18:54:42 +0530

    init

---
+++ ekam.js
@@ -0,0 +1,20 @@
+// ekam-deep-singh kebab case
+
+// upper case
+// EKAM-DEEP-SING
+
+
+// lower case
+// ekam deep singh
+
+
+
+// camel case
+// ekamDeepSingh
+
+
+// pascal case
+// EkamDeepSingh
+
+// snake case
+// ekam_deep_singh
